cbf9da51c1ffd57d3c88ae36c0462bad55b94ee7
Giwoo Lee <gglee89@gmail.com>
2021-02-22 18:20:03 +0900

    feat: Include Feature #1

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -101,9 +101,9 @@ export default function Album() {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link to="/pricing">
+                  <Link to="/calendar">
                     <Button variant="outlined" color="primary">
-                      Pricing
+                      Calendar
                     </Button>
                   </Link>
                 </Grid>
