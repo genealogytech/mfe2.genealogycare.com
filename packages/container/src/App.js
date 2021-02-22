589e9cf6f2f06652e59523536eb2f2cb6fe66902
Giwoo Lee <gglee89@gmail.com>
2021-02-22 14:32:13 +0900

    init: Add comments in App.js to run the pipeline

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import { mount as marketingMount } from 'marketing/MarketingApp';
 
+// COMPONENTS
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
