e03733b479dc960941c0118559d02071b20c704b
Giwoo Lee <gglee89@gmail.com>
2021-02-23 14:08:29 +0900

    feat: Setup routing for the Marketing and Container

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,14 +1,35 @@
-import { mount as marketingMount } from 'marketing/MarketingApp';
+import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
-  // Run only once
+  // Run every time this component is
+  // updated or changed in any way
+  // The way we limit how often it executes
+  // is with using the dependency array
+  // [] - empty array (executes only one time)
   useEffect(() => {
     // Allow mount to get a DYNAMIC reference
     // to the existing rendered HTML element
-    marketingMount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathName }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathName) {
+          // nextPathName
+          // The path the Marketing App is trying
+          // to navigate to.
+          history.push(nextPathName);
+        }
+      },
+    });
+
+    // Everytime there's any change to our browser history,
+    // we want to call onParentNavigate
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import App from './App';
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory
+    ? createBrowserHistory()
+    : createMemoryHistory();
+
+  // We then create a memory history object and we tell
+  // the memory history object that whenever the URL changes
+  // whenever the path changes, we want to automatically
+  // call the onNavigate function.
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation
@@ -13,7 +36,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRootEl = document.querySelector('#_marketing-dev-root');
 
   if (devRootEl) {
-    mount(devRootEl);
+    mount(devRootEl, { defaultHistory: createBrowserHistory() });
   }
 }
 
